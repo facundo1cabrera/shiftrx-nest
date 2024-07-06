c5041b41c7d0006ca66d04da93f183b1c1862a63
facundo1cabrera <cabrera.facundo990@gmail.com>
2024-07-06 22:34:52 +0200

    feat: add expire time in token responses

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,8 @@ import { UserService } from 'src/user/user.service';
 import { compare } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 
+const EXPIRE_TIME = 5 * 60 *60 * 1000;
+
 @Injectable()
 export class AuthService {
 
@@ -24,13 +26,14 @@ export class AuthService {
             user,
             backendTokens: {
                 accessToken: await this.jwtService.signAsync(payload, {
-                    expiresIn: '1h',
+                    expiresIn: '5h',
                     secret: process.env.JWT_SECRET_KEY
                 }),
                 refreshToken: await this.jwtService.signAsync(payload, {
                     expiresIn: '7d',
                     secret: process.env.JWT_REFRESH_KEY
-                })
+                }),
+                expiresIn: new Date().setTime(new Date().getTime() + EXPIRE_TIME)
             }
         }
 
@@ -55,13 +58,14 @@ export class AuthService {
 
         return {
             accessToken: await this.jwtService.signAsync(payload, {
-                expiresIn: '1h',
+                expiresIn: '5h',
                 secret: process.env.JWT_SECRET_KEY
             }),
             refreshToken: await this.jwtService.signAsync(payload, {
                 expiresIn: '7d',
                 secret: process.env.JWT_REFRESH_KEY
-            })
+            }),
+            expiresIn: new Date().setTime(new Date().getTime() + EXPIRE_TIME)
         }
     }
 }
