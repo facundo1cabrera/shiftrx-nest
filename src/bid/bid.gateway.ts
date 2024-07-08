7930464abced3023f2a80e03e7cb702c5beeefe6
facundo1cabrera <cabrera.facundo990@gmail.com>
2024-07-08 08:30:58 +0200

    feat: add websocket gateway for bids

---
+++ src/bid/bid.gateway.ts
@@ -0,0 +1,29 @@
+import { OnModuleInit, UseGuards } from '@nestjs/common';
+import { SubscribeMessage, WebSocketGateway, ConnectedSocket, MessageBody, WebSocketServer } from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+
+@WebSocketGateway({
+  cors: '*'
+})
+export class BidGateway implements OnModuleInit {
+
+  @WebSocketServer()
+  public server: Server;
+
+  onModuleInit() {
+    this.server.on("connection", (socket: Socket) => {
+      console.log("client connected", socket.handshake.headers);
+
+      this.server.on("disconnect", () => {
+        console.log("client disconnected ", socket.id);
+      })
+    })
+  }
+
+  @UseGuards(JwtGuard)
+  @SubscribeMessage('on-bid-placed')
+  handleOnBidPlaced(@MessageBody() bid: any) {
+    this.server.emit('on-bid-placed', bid);
+  }
+}
--- src/bid/bid.module.ts
+++ src/bid/bid.module.ts
@@ -5,9 +5,10 @@ import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
 import { UserService } from 'src/user/user.service';
 import { AuctionService } from 'src/auction/auction.service';
+import { BidGateway } from './bid.gateway';
 
 @Module({
   controllers: [BidController],
-  providers: [BidService, PrismaService, JwtService, UserService, AuctionService]
+  providers: [BidService, PrismaService, JwtService, UserService, AuctionService, BidGateway]
 })
 export class BidModule {}
